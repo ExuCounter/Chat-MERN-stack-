20c8d15f3bb2083bf97b1101549e007057052041
Volodymyr Potiichuk <exucounter@gmail.com>
2020-09-26 14:10:53 +0300

    Auth Page switch statement for non-auth users

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 export const AuthPage = (props) => {
-    return ( 
-        <div>{props.step}</div>
-    );
+    let step = props.step;
+    switch(step){
+        case 'login': 
+        return(
+            AuthPageContent('login')
+        )
+        case 'register': 
+        return(
+            AuthPageContent('register')
+        )
+        case 'reset-password': 
+        return(
+            AuthPageContent('reset-password')
+        )
+    }
 }
