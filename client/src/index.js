ae324e2dc994855eb39e5d9980c3f4d9c6d55368
Volodymyr Potiichuk <exucounter@gmail.com>
2020-09-26 16:34:59 +0300

    Styles structure

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
+import {Row, Col} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {AuthSidebar} from '../components/auth/auth-sidebar/sidebar';
 import {AuthContent} from '../components/auth/auth-content/content';
+import {Row, Col, Container} from 'react-bootstrap';
+import '../styles/auth/auth.css';
 
 export const AuthPage = (props) => {
     return(
-        <React.Fragment>
-            <AuthSidebar/>
-            <AuthContent step={props.step}/>
-        </React.Fragment>
+        <Container fluid>
+            <Row>
+                <Col sm={4} className='no-padding'>
+                    <AuthSidebar/>
+                </Col>
+                <Col sm={8} className='no-padding'>
+                    <AuthContent step={props.step}/>
+                </Col>
+            </Row>
+        </Container>
     )
 }
